58e170a5254bd61b17cfeb95445653c1c3e3c1e1
Patricolaclear <alizee.patricola@epitech.eu>
2021-10-27 01:02:11 +0200

    test recuperate info user

--- my_redditech/lib/screens/sub_page_profil/sub_profil.dart
+++ my_redditech/lib/screens/sub_page_profil/sub_profil.dart
@@ -1,6 +1,9 @@
+import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:my_redditech/states/global_state.dart';
+import 'package:my_redditech/states/subreddits_state.dart';
 import 'package:provider/provider.dart';
+import 'package:my_redditech/states/subreddits_state.dart';
 
 class SubProfil extends StatelessWidget {
   const SubProfil({Key? key, required this.name, required this.color})
@@ -21,11 +24,11 @@ class SubProfil extends StatelessWidget {
 class PageProfil extends StatelessWidget {
   const PageProfil({Key? key, required this.name, required this.color})
       : super(key: key);
-
   final String name;
   final Color color;
   @override
   Widget build(BuildContext context) {
+  Future<Redditor> Function() redditor = () => Provider.of<GlobalState>(context, listen: false).getUserInfo();
     const urlImage =
         'https://media-exp1.licdn.com/dms/image/C5603AQFrdq9exFkovQ/profile-displayphoto-shrink_800_800/0/1631048636825?e=1640822400&v=beta&t=6okkS6MWfH8prWG8C5wB5uboQ31FtP78K1T3O9Irngk';
 
@@ -40,7 +43,7 @@ class PageProfil extends StatelessWidget {
                   maxExtent: 40,
                   currentExtent: 200,
                   child: CircleAvatar(
-                      radius: 30, backgroundImage: NetworkImage(urlImage))),
+                      radius: 30, backgroundImage: NetworkImage(redditor.))),
               foregroundColor: Colors.black,
               centerTitle: true,
               backgroundColor: color),
